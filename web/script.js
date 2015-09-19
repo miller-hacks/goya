b352496d77e6fdd599eda06614025efdff95b513
Alexander Zhebrak <fata2ex@gmail.com>
2015-09-19 14:40:53 +0300

    canvas image initial

--- web/script.js
+++ web/script.js
@@ -11,10 +11,28 @@ $(function() {
 				contentType: "application/json; charset=utf-8",
 				data: data,
 				success: function(data) {
-					console.log(data);
+					(function draw(imageUrl, faces) {
+						var ctx = document.getElementById('canvas').getContext('2d');
+						var img = new Image();
 
-					$('#image').attr('src', imageUrl);
-					$('#faceCoordinates').text(data);
+						img.onload = function() {
+							console.log('adada');
+							console.log(imageUrl);
+							for (var idx=0; idx<=faces.len; idx++) {
+								console.log(idx);
+								var obj = faces[idx];
+								ctx.drawImage(img,0,0);
+								ctx.beginPath();
+								ctx.moveTo(obj.x, obj.y);
+								
+								ctx.lineTo(obj.x + obj.height, obj.y);
+								ctx.lineTo(obj.x, obj.y + obj.width);
+
+								ctx.stroke();
+							}
+						}
+						img.src = imageUrl;							
+					})(imageUrl, data.faces)
 				}
 			})
 		}
