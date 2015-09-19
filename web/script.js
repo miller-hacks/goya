878501cce18071f90415a4e3ef6a608a7523ff86
Alexander Zhebrak <fata2ex@gmail.com>
2015-09-19 14:07:14 +0300

    ajax json sending url

--- web/script.js
+++ web/script.js
@@ -3,8 +3,16 @@ $(function() {
 		var imageUrl = $('#imageUrlInput').val();
 		console.log(imageUrl);
 		if (imageUrl.length) {
-			$.ajax('/', {imageUrl: imageUrl}, function(data) {
-				console.log(data);
+			var data = JSON.stringify({url: imageUrl});
+
+			$.ajax({
+				type: 'POST',
+				url: '/',
+				contentType: "application/json; charset=utf-8",
+				data: data,
+				success: function(data) {
+					console.log(data);
+				}
 			})
 		}
 	});
