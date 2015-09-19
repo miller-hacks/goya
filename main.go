41d4471c7a299e44c73bcfb01a3707465a3a6813
Andrey Kostakov <a.kostakov@corp.mail.ru>
2015-09-19 13:02:05 +0300

    simple web server

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type RequestJSON struct {
+	URL string
+}
+
+func upload_handler(w http.ResponseWriter, r *http.Request) {
+	log.Println("request!")
+	body, _ := ioutil.ReadAll(r.Body)
+	log.Println(string(body))
+	defer r.Body.Close()
+	var rj = RequestJSON{}
+	err := json.Unmarshal(body, &rj)
+	if err != nil {
+		log.Println("error!")
+	}
+	log.Println(rj)
+}
+
+func main() {
+	http.HandleFunc("/", upload_handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
