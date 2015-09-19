087f990f7d37e486d350773cfab5056ae723d855
Alexandr Emelin <frvzmb@gmail.com>
2015-09-19 12:52:00 +0300

    detect faces

---
+++ detect.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bytes"
+	"image"
+	"io"
+	"log"
+	"path"
+	"runtime"
+
+	"github.com/lazywei/go-opencv/opencv"
+)
+
+type Face struct {
+	PointX int
+	PointY int
+	Width  int
+	Height int
+}
+
+func currentDir() string {
+	_, currentfile, _, _ := runtime.Caller(0)
+	return path.Dir(currentfile)
+}
+
+func detect(cascade *opencv.HaarCascade, r io.Reader) ([]*Face, error) {
+	img, format, err := image.Decode(r)
+	if err != nil {
+		return []*Face{}, err
+	}
+	log.Printf("Image format: %s", format)
+	rects := cascade.DetectObjects(img)
+
+	faces := []*Face{}
+
+	for _, value := range rects {
+		face := &Face{
+			PointX: value.X(),
+			PointY: value.Y(),
+			Width:  value.Width(),
+			Height: value.Height(),
+		}
+		faces = append(faces, face)
+	}
+	return faces, nil
+}
+
+func faces(r io.Reader) ([]*Face, error) {
+	cascade := opencv.LoadHaarClassifierCascade(path.Join(currentDir(), "haarcascade_frontalface_alt.xml"))
+	return detect(cascade, r)
+}
